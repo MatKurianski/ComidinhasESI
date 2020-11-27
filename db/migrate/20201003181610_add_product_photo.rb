c0492e6cc2523609b11834d4b755e54ca3abfd3a
Matheus Kurianski <matheuskurianski@usp.br>
2020-11-27 10:52:26 -0300

    Remove resquícios do Paperclip

--- db/migrate/20201003181610_add_product_photo.rb
+++
@@ -1,9 +0,0 @@
-class AddProductPhoto < ActiveRecord::Migration[6.0]
-  def self.up
-    add_attachment :products, :photo
-  end
-
-  def self.down
-    remove_attachment :products, :photo
-  end
-end
